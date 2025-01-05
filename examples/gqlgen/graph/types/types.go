016073baf36ff0a6f317b1ecad4d619b0344b68d
Asger Noer <asger@noer.dev>
2025-01-06 09:18:43 +1300

    chore: removing unneeded bindings from gqlgen example

--- examples/gqlgen/graph/types/types.go
+++ examples/gqlgen/graph/types/types.go
@@ -6,8 +6,6 @@ import (
 	"github.com/asger-noer/go-cursor"
 )
 
-type PageInfo = cursor.PageInfo
-
 type (
 	TodoConnection = cursor.Connection[model.Todo]
 	TodoEdge       = cursor.Edge[model.Todo]
